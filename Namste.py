e032a3bc67d1732b83c7af043c3ab5b4ead0c02b
Pradeep <prdshukla@gmail.com>
2023-08-02 10:59:50 -0500

    pushed to github

--- Namste.py
+++ Namste.py
@@ -1,1 +1,10 @@
 print("namaste")
+
+print("namaste visitor " + str(10))
+
+
+def greetings(name, visitorNo):
+    print("namaste " + name)
+    print("namaste visitor " + str(visitorNo))
+
+greetings("panda", 9)
