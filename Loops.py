7933a674d17f1615387d77e6599ab6dec8c7ca99
Pradeep <prdshukla@gmail.com>
2023-08-05 21:02:36 -0500

    updated comments.

--- Loops.py
+++ Loops.py
@@ -1,11 +1,10 @@
 def to_celcius(temp):
     return (temp-32)*5/9
 
-
-
+# function implemetation
 print (to_celcius(453))
 
-
+# string manipulation
 for left in range(9):
     for right in range(left,9):
         print("["+str(right) + "|" +str(left) + "]", end=" ")
